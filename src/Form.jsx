42c335a50cd31a995cc6d1b055c6af3b0d2f2d9e
Varunraje <varunraje47@gmail.com>
2023-06-02 06:19:08 +0530

    Email  input Added

--- src/Form.jsx
+++ src/Form.jsx
@@ -9,45 +9,46 @@ const Form = () => {
   const [age, setAge] = useState("")
   const [profession, setProfession] = useState("")
   const [image, setImage] = useState("")
+  const [email, setEmail] = useState("")
   const [instagram, setInstagram] = useState("")
   const [linkedin, setLinkedin] = useState("")
   const [github, setGithub] = useState("")
   const [whatsapp, setWhasapp] = useState("")
 
   async function makecard(e) {
- 
+
     let data = { name, age, profession, image, instagram, linkedin, github, whatsapp }
-    
-    if(name&&profession&&image&&whatsapp){
-      var x=await adddata();
-    navigate("/ocards");
+
+    if (name && profession && image && whatsapp) {
+      var x = await adddata();
+      navigate("/ocards");
     }
-    else{
+    else {
       alert("Please fill the Form correctly!");
     }
     // e.preventDefault()
   }
 
- 
-  const adddata=async()=>{
-  try{
-    var x=await database.createDocument(process.env.REACT_APP_DATABASE_ID,process.env.REACT_APP_COLL_ID,"unique()",{
-    name:name,
-    age:age,
-    profession:profession,
-    email:"pranjal.maurya402@gmail.com",
-    photo:"https://www.google.com",
-    insta:instagram,
-    linkedin:linkedin,
-    github:github,
-    whatsapp:whatsapp
-  }); 
-  localStorage.setItem("id",x.$id);
-  console.log(x);
-}
-catch(e){
-  console.log(e);
-}  
+
+  const adddata = async () => {
+    try {
+      var x = await database.createDocument(process.env.REACT_APP_DATABASE_ID, process.env.REACT_APP_COLL_ID, "unique()", {
+        name: name,
+        age: age,
+        profession: profession,
+        email: "pranjal.maurya402@gmail.com",
+        photo: "https://www.google.com",
+        insta: instagram,
+        linkedin: linkedin,
+        github: github,
+        whatsapp: whatsapp
+      });
+      localStorage.setItem("id", x.$id);
+      console.log(x);
+    }
+    catch (e) {
+      console.log(e);
+    }
   }
   return (
     <>
@@ -82,6 +83,18 @@ catch(e){
               value={profession}
               required />
             <label className="absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Profession</label>
+          </div>
+
+          <div className="relative z-0 mb-6 group">
+            <input
+              type="text"
+              name=''
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required />
+            <label className="absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
           </div>
 
           <div className="relative z-0 mb-6 group">
@@ -140,11 +153,11 @@ catch(e){
         </div>
         <div className='flex justify-center'>
 
-          <button onClick={(e) => { 
-               e.preventDefault();
+          <button onClick={(e) => {
+            e.preventDefault();
             makecard();
-             
-            }} type="submit" className=' bg-blue-500 p-3 rounded-md text-black hover:text-white hover:bg-blue-300 cursor-pointer'>Get Your Ocard</button>
+
+          }} type="submit" className=' bg-blue-500 p-3 rounded-md text-black hover:text-white hover:bg-blue-300 cursor-pointer'>Get Your Ocard</button>
         </div>
 
       </form>
