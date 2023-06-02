9238d9b0dd072ee4e5bd7115a2c2aae910f6f900
Varunraje <varunraje47@gmail.com>
2023-06-02 06:19:50 +0530

    Email  icon on cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 import ManThinking from '../ManThinking.png'
 import { AiOutlineInstagram, AiOutlineWhatsApp, AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 
 const Cards = () => {
   return (
     <>
-      <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-        <a href='/'>
-          <img class="rounded-t-lg" src={ManThinking} alt="" />
-        </a>
-        <div class="p-5 text-black text-center">
-          <a href="/">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pranjal Maurya</h5>
-          </a>
-          <p class="mb-3 font-semibold">Enterpreneur</p>
-          <p class="mb-3 font-medium">Male ,20</p>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 cursor-pointer">
+        <img className="rounded-t-lg" src={ManThinking} alt="" />
+        <div className="p-5 text-black text-center">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Pranjal Maurya</h5>
+          <p className="mb-3 font-semibold">Enterpreneur</p>
+          <p className="mb-3 font-medium">Male ,20</p>
         </div>
         <div className='flex justify-center gap-3 py-2'>
           <a href='/'><AiOutlineInstagram size={42} className='text-pink-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><AiOutlineWhatsApp size={42} className='text-green-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><FaLinkedinIn size={42} className='text-blue-700 rounded bg-white p-1 cursor-pointer' /></a>
+          <a href='/'><SiGmail size={42} className='text-red-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><AiFillGithub size={42} className='rounded bg-black p-1 cursor-pointer' /></a>
         </div>
       </div>
--- src/components/Cards1.jsx
+++ src/components/Cards1.jsx
@@ -2,25 +2,24 @@ import React from 'react'
 import ManThinking from '../ManThinking.png'
 import { AiOutlineInstagram, AiOutlineWhatsApp, AiFillGithub } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+
 
 const Cards1 = () => {
   return (
     <>
-      <div className="max-w-sm border border-gray-200 rounded-lg shadow bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 ">
-        <a href='/'>
-          <img className="rounded" src={ManThinking} alt="" />
-        </a>
+      <div className="max-w-sm border border-gray-200 rounded-lg shadow bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 cursor-pointer">
+        <img className="rounded" src={ManThinking} alt="" />
         <div className="p-5 text-black text-center">
-          <a href="/">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Varunraje</h5>
-          </a>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Varunraje</h5>
           <p className="mb-3 font-normal">Developer</p>
           <p className="mb-3 font-normal">Male, 20</p>
         </div>
-        <div className='flex justify-center gap-3'>
+        <div className='flex justify-center gap-3 py-2'>
           <a href='/'><AiOutlineInstagram size={42} className='text-pink-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><AiOutlineWhatsApp size={42} className='text-green-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><FaLinkedinIn size={42} className='text-blue-700 rounded bg-white p-1 cursor-pointer' /></a>
+          <a href='/'><SiGmail size={42} className='text-red-700 rounded bg-white p-1 cursor-pointer' /></a>
           <a href='/'><AiFillGithub size={42} className='rounded bg-black p-1 cursor-pointer' /></a>
         </div>
       </div>
